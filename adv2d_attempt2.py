9757705d99d04571e676f12276ea5dadc38ddeab
Prashant Kumar <prashant_1111@hotmail.com>
2023-06-15 15:16:47 +0200

    Comparing different schemes

--- adv2d_attempt2.py
+++ adv2d_attempt2.py
@@ -19,8 +19,8 @@ def kerDWendland(r,h, DIM):
         return qr
 
 def Lagrangian(args):
-    i, particle_coords, vel, h, dt, Conc, vol, NN_idx, DIM, Conc_FD = args
-    C_nn = [0, 0]
+    i, particle_coords, vel, h, dt, Conc, Conc_N_CD, Conc_N_SPH, Conc_UP, Conc_N_UP,vol, NN_idx, DIM, Conc_CD = args
+    C_nn = [0, 0, 0, 0,0,0]
     if DIM == 2 and (particle_coords[i,0]<0.2 or particle_coords[i,0]>0.8 or particle_coords[i,1]<0.001 or particle_coords[i,1]>0.009):
         return C_nn
     if DIM == 1 and (particle_coords[i,0]<0.1 or particle_coords[i,0]>1.9):
@@ -34,6 +34,7 @@ def Lagrangian(args):
         dw = kerDWendland(r,h, DIM)
         dot = np.dot(dr,vel[i])
         concx = Conc[j] - Conc[i]
+        concx_N_SPH = Conc_N_SPH[j] - Conc_N_SPH[i]
         # directional change in concentration
         # temp_j = dw * vol * Conc[j]* dot/r
         # temp_i = dw * vol * Conc[i]* dot/r
@@ -43,21 +44,27 @@ def Lagrangian(args):
         #     C_nn += temp_j
         # non directional change in concentration
         adv_component = concx * vol * dw  * dot/r
+        adv_component_N = concx_N_SPH * vol * dw  * dot/r
         C_nn[0] += adv_component
+        C_nn[1] += adv_component_N
+    #CD methods
     dr = particle_coords[i,0]-particle_coords[i-1,0]
-    C_nn[1] = vel[i,0]*(Conc_FD[i]-Conc_FD[i-1])/dr
+    C_nn[2] = vel[i,0]*(Conc_CD[i+1]-Conc_CD[i-1])/(2*dr)
+    C_nn[3] = vel[i,0]*(Conc_N_CD[i+1]-Conc_N_CD[i-1])/(2*dr)
+    C_nn[4] = vel[i,0]*(Conc_UP[i]-Conc_UP[i-1])/(dr)
+    C_nn[5] = vel[i,0]*(Conc_N_UP[i]-Conc_N_UP[i-1])/(dr)
     return C_nn
 
 
 def main():
     # plt.style.use('dark_background')
     DIM = 1
-    dp = 0.001
+    dp = 0.002
 
-    dt = 0.01
-    t_end = 5
+    dt = 0.001
+    t_end = 100
     if DIM ==1:
-        h = dp*9
+        h = dp*8
         boxsize = (2,dp)
     if DIM ==2:
         h = dp*2
@@ -85,26 +92,34 @@ def main():
     middle_particles = np.where(particle_coords[:,1]==y_coords[mid_y])
 
     #initialize the velocity of the particles
-    #Single velocity
-    # vel = np.array([0.01,0])
-    #Multiple velocities
     vel = np.zeros((num_particles,2))
 
 
     mean = boxsize[0]/4
     #initialize the concentration of the particles
     Conc = np.zeros(len(particle_coords))
-    Conc_FD = np.zeros(len(particle_coords))
+    Conc_CD = np.zeros(len(particle_coords))
+    Conc_UP = np.zeros(len(particle_coords))
+    Conc_N_SPH = np.zeros(len(particle_coords))
+    Conc_N_CD = np.zeros(len(particle_coords))
+    Conc_N_UP = np.zeros(len(particle_coords))
     ConcOrig = np.zeros(len(particle_coords))
     for i in range(len(particle_coords)):
         Conc[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
         ConcOrig[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
-        Conc_FD[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
-        # if particle_coords[i,0]> 0.2 and particle_coords[i,0]<0.3:
+        Conc_CD[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
+        Conc_UP[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
+        Conc_N_SPH[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
+        Conc_N_CD[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
+        Conc_N_UP[i] = math.exp(-((particle_coords[i,0]-mean)**2)/0.005)
+        # if particle_coords[i,0] < 0.5 and particle_coords[i,0] > 0.4:
         #     Conc[i] = 1
         #     ConcOrig[i] = 1
-        # vel[i,0] = 0.01
-        vel[i,0] = particle_coords[i,0] * (0.1)
+        #     Conc_CD[i] = 1
+        #     Conc_N_SPH[i] = 1
+        #     Conc_N_CD[i] = 1
+        vel[i,0] = 0.01
+        vel[i,0] = (particle_coords[i,0]+0.01) * (0.001)
         # vel[i,0] = math.exp(-((particle_coords[i,0]-(boxsize[0]/2))**2)/0.5)*0.01
     
     # volume of the particles
@@ -125,10 +140,11 @@ def main():
     count = 0
     count_normal = 0
     while(t<t_end):
-        print("time = ", t)
-        total_C_before = np.sum(Conc)*vol*num_particles
-        C_temp = np.zeros(len(Conc))
-        C_FD_temp = np.zeros(len(Conc))
+
+        total_C_before_CD = np.sum(Conc_N_CD)*vol*num_particles
+        total_C_before_SPH = np.sum(Conc_N_SPH)*vol*num_particles
+        total_C_before_UP = np.sum(Conc_N_SPH)*vol*num_particles
+
         if single_thread:
             for i in range(num_particles):
                 args = (i, particle_coords, vel, h, dt, Conc, vol, NN_idx, DIM)
@@ -136,19 +152,32 @@ def main():
         elif not single_thread:
             num_processes = mp.cpu_count()-6
             with mp.Pool(num_processes) as pool:
-                ranges = [(i, particle_coords, vel, h, dt, Conc, vol, NN_idx, DIM, Conc_FD) for i in range(num_particles)]
+                ranges = [(i, particle_coords, vel, h, dt, Conc, Conc_N_CD, Conc_N_SPH,  Conc_UP, Conc_N_UP,vol, NN_idx, DIM, Conc_CD) for i in range(num_particles)]
                 C_temp = pool.map(Lagrangian, ranges)
 
         C_temp = np.array(C_temp)
         C_temp = C_temp*dt
+
         Conc -= C_temp[:, 0]
-        Conc_FD -= C_temp[:, 1]
-        total_C_after = np.sum(Conc)*vol*num_particles
-        # if count_normal % 100 == 0:
-            # Conc = Conc/total_C_after*total_C_before
-            # print("here")
+        Conc_N_SPH -= C_temp[:, 1]
+        Conc_CD -= C_temp[:, 2]
+        Conc_N_CD -= C_temp[:, 3]
+
+        Conc_UP -= C_temp[:, 2]
+        Conc_N_UP -= C_temp[:, 3]
+
+        total_C_after_CD = np.sum(Conc_N_CD)*vol*num_particles
+        Conc_N_CD = Conc_N_CD/total_C_after_CD*total_C_before_CD
+
+        total_C_after_SPH = np.sum(Conc_N_SPH)*vol*num_particles
+        Conc_N_SPH = Conc_N_SPH/total_C_after_SPH*total_C_before_SPH
+
+        total_C_after_UP = np.sum(Conc_N_UP)*vol*num_particles
+        Conc_N_UP = Conc_N_UP/total_C_after_UP*total_C_before_UP
+
         count_normal += 1
         t += dt
+        print("time = ", f'{t:.3f}', "SPH: ", sum(ConcOrig)-sum(Conc), "CD: ", sum(ConcOrig)-sum(Conc_CD), "UP: ", sum(ConcOrig)-sum(Conc_UP))
 
         if(t%0.01<dt and plot_state):
             t_precision = round(t,2)
@@ -183,40 +212,79 @@ def main():
                 # plt.pause(0.01)
 
     # Difference between initial and final concentration
-    print("Difference between initial and final concentration: ", np.sum(ConcOrig-Conc))
+    print("Difference between initial and final concentration_SPH: ", np.sum(ConcOrig-Conc))
+    print("Difference between initial and final concentration_CD: ", np.sum(ConcOrig-Conc_CD))
+    print("Difference between initial and final concentration_UP: ", np.sum(ConcOrig-Conc_UP))
+    print("Difference between initial and final concentration_SPH_N_CD: ", np.sum(ConcOrig-Conc_N_CD))
+    print("Difference between initial and final concentration_SPH_N_SPH: ", np.sum(ConcOrig-Conc_N_SPH))
+    print("Difference between initial and final concentration_SPH_N_UP: ", np.sum(ConcOrig-Conc_N_UP))
 
-    # Subplots to compare the initial, final, and concentration difference with colorbar
     fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 1]})
-    ini_plot = ax1.scatter(particle_coords[:,0], particle_coords[:,1], c=ConcOrig)
+
+    plt.clf()
+    fig, (ax1, ax2) = plt.subplots(2, 1, gridspec_kw={'height_ratios': [1, 1]})
+    ini_plot = ax1.scatter(particle_coords[:,0], particle_coords[:,1], c=ConcOrig, s = (ConcOrig*100))
     ax1.set_title("Initial concentration")
+    ax1.set_ylim(-0.01,0.01)
     plt.colorbar(ini_plot, ax=ax1)
     ax1.grid()
-    Final_plot = ax2.scatter(particle_coords[:,0], particle_coords[:,1], c=Conc)
+    Final_plot = ax2.scatter(particle_coords[:,0], particle_coords[:,1], c=Conc, s = Conc*100)
     ax2.set_title("Final concentration")
+    ax2.set_ylim(-0.01,0.01)
     plt.colorbar(Final_plot, ax=ax2)
     ax2.grid()
+    plt.tight_layout()
     plt.savefig("adv2d_"+str(DIM)+".png", dpi=300)
     
     plt.clf()
     plt.plot(particle_coords[middle_particles,0].T, ConcOrig[middle_particles], 'r*',markevery = 10, label="Initial", fillstyle='none' )
-    plt.plot(particle_coords[middle_particles,0].T, Conc_FD[middle_particles],'b', label="FD")
-    plt.plot(particle_coords[middle_particles,0].T, Conc[middle_particles],'g', label="Final")
+    plt.plot(particle_coords[middle_particles,0].T, Conc_CD[middle_particles],'b*', markevery=10, label="CD", fillstyle='none')
+    plt.plot(particle_coords[middle_particles,0].T, Conc[middle_particles],'b:', label="Final")
+    plt.plot(particle_coords[middle_particles,0].T, Conc_UP[middle_particles],'b^', markevery=12, label="UP", fillstyle='none')
     plt.ylim(-0.1,1.1)
     plt.title("Concentration along the middle line")
     plt.xlabel("x position")
     plt.ylabel("Concentration")
     plt.legend()
     plt.grid()
-    plt.savefig("adv2d_line_"+str(DIM)+".png", dpi=300)
-    print(ConcOrig.shape)
-    print(Conc.shape)
-    print(particle_coords[middle_particles,0].shape)
+    plt.savefig("adv2d_lineNN_"+str(DIM)+".png", dpi=300)
+    plt.clf()
+    plt.plot(particle_coords[middle_particles,0].T, ConcOrig[middle_particles], 'r*',markevery = 10, label="Initial", fillstyle='none' )
+    plt.plot(particle_coords[middle_particles,0].T, Conc_N_CD[middle_particles],'g*', markevery=10, label="CD_N", fillstyle='none')
+    plt.plot(particle_coords[middle_particles,0].T, Conc_N_SPH[middle_particles],'g:', label="Final_N")
+    plt.plot(particle_coords[middle_particles,0].T, Conc_N_UP[middle_particles],'g^', markevery=12, label="UP_N", fillstyle='none')
+    plt.ylim(-0.1,1.1)
+    plt.title("Concentration along the middle line")
+    plt.xlabel("x position")
+    plt.ylabel("Concentration")
+    plt.legend()
+    plt.grid()
+    plt.savefig("adv2d_lineN_"+str(DIM)+".png", dpi=300)
+    # print(ConcOrig.shape)
+    # print(Conc.shape)
+    # print(particle_coords[middle_particles,0].shape)
 
 
 
     # difference between the peak position of Conc and ConcOrig
-    print("Difference between the peak position of Conc and ConcOrig: ", (np.argmax(Conc)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_NN)SPH and ConcOrig: ", (np.argmax(Conc)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_CD and ConcOrig: ", (np.argmax(Conc_CD)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_N_CD and ConcOrig: ", (np.argmax(Conc_N_CD)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_N_SPH and ConcOrig: ", (np.argmax(Conc_N_SPH)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_UP and ConcOrig: ", (np.argmax(Conc_UP)-np.argmax(ConcOrig))*dp)
+    print("Difference between the peak position of Conc_N_UP and ConcOrig: ", (np.argmax(Conc_N_UP)-np.argmax(ConcOrig))*dp)
     print("Final position of the peak of Conc: ", particle_coords[np.argmax(Conc[middle_particles])])
+
+
+    # save the 7 Conc arrays to separate csv files
+    np.savetxt("ConcOrig.csv", np.array(particle_coords[middle_particles,0].T,ConcOrig[middle_particles]), delimiter=",")
+    np.savetxt("Conc.csv", np.array(particle_coords[middle_particles,0].T,Conc[middle_particles]), delimiter=",")
+    np.savetxt("Conc_CD.csv", np.array(particle_coords[middle_particles,0].T,Conc_CD[middle_particles]), delimiter=",")
+    np.savetxt("Conc_N_CD.csv", np.array(particle_coords[middle_particles,0].T,Conc_N_CD[middle_particles]), delimiter=",")
+    np.savetxt("Conc_N_SPH.csv", np.array(particle_coords[middle_particles,0].T,Conc_N_SPH[middle_particles]), delimiter=",")
+    np.savetxt("Conc_UP.csv", np.array(particle_coords[middle_particles,0].T,Conc_UP[middle_particles]), delimiter=",")
+    np.savetxt("Conc_N_UP.csv", np.array(particle_coords[middle_particles,0].T,Conc_N_UP[middle_particles]), delimiter=",")
+
 
 
 
@@ -228,3 +296,4 @@ if __name__ == '__main__':
 # using ffmpeg to make a video from the images
 # https://askubuntu.com/questions/610903/how-can-i-create-a-video-file-from-a-set-of-jpg-images
 # ffmpeg -framerate 10 -i line_%00d.png -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p output.mp4
+# 
